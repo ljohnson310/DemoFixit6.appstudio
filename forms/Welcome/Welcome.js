4cf440f2410238762a73619d7aafc8d87d04184c
ljohnson310 <60620143+ljohnson310@users.noreply.github.com>
2020-02-03 16:34:18 -0600

    Update Welcome.js
    this commit adds code to show an alert with second person

--- forms/Welcome/Welcome.js
+++ forms/Welcome/Welcome.js
@@ -4,4 +4,5 @@ let names = ["Bob", "Mary", "Sue"]
 btnShowMessage.onclick=function(){
   console.log(`My best friend is ${names[0]}`)
   lblMessage.textContent = `Another Friend is ${names[2]}`
+  alert (The second friend is named ${names[2]}`.)
 }
